728528f40b1432889e8883c29a7714fb524571d9
ayame30 <alnaym30@gmail.com>
2015-08-17 10:35:37 +0800

    sign-up-now.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,7 @@
 	=Gender options
 	=Country options
 	=Form validate
+	=Tag form validate
 	=Step 1 validate
 	=Step 2 validate
 	=Social check binding
@@ -54,6 +55,14 @@ $(document).ready(function(){
 		}
 	});
 
+	$('.js-step-1-nav').on('click',function() {
+		$('.js-step-2-nav').addClass('disable');
+		$('.js-step-3-nav').addClass('disable');
+	});
+	$('.js-step-2-nav').on('click',function() {
+		$('.js-step-3-nav').addClass('disable');
+	});
+
 	/*	=Form validate
 	-------------------------------------------------- */
 	if($('body').hasClass('form')) {
@@ -83,9 +92,20 @@ $(document).ready(function(){
 		});
 	}
 
+	/*	=Tag form validate
+	-------------------------------------------------- */
+	$('.tag-form-group input').on('blur',function() {
+		if($(this).parents('.tag-form-group').find('.tag').length == 0) {
+			$(this).parents('.tag-form-group').next('.with-errors').html('<ul class="list-unstyled"><li>Please input at least one tag</li></ul>');
+		} else {
+			$(this).parents('.tag-form-group').next('.with-errors').html('');
+		}
+	});
+
 	/*	=Step 1 validate
 	-------------------------------------------------- */
-	$('.js-step-1-continue').on('click', function() {
+	$('.js-step-1-continue').on('click',checkStep1 );
+	function checkStep1() {
 		// 1. Do validator
 		var hasErrorMsg = $('.js-step-1 .with-errors ul').length;
 		var blankQty = 0;
@@ -101,20 +121,25 @@ $(document).ready(function(){
 			$('.js-step-2-nav').trigger('click');
 			$("html, body").animate({ scrollTop: 0 }, "slow");
 			$(window).bind('beforeunload', function(){
-        return 'You have not completed the application process.';
-      });
+				return 'You have not completed the application process.';
+			});
 		} else {
 		  validateSelection();
 		  $('form').submit();
-		  // $('html, body').animate({
-	   //      // scrollTop: $('.js-step-1').offset().top
-	   //  }, 500);
+		  $('html, body').animate({
+	    	scrollTop: $('.js-step-1 .with-errors ul').closest('article').offset().top
+	      }, 500);
 		}
-	});
+	}
 
 	/*	=Step 2 validate
 	-------------------------------------------------- */
+	var checkedOptions = 0;
+	window.b = checkedOptions;
+
 	$('.js-step-2-continue').on('click', function() {
+		validateStep2(checkedOptions);
+
 		// 1. Do validator
 		var hasErrorMsg = $('.js-step-2 .with-errors ul').length;
 		var blankQty = 0;
@@ -130,9 +155,9 @@ $(document).ready(function(){
 			$('.js-step-3-nav').removeClass('disable');
 			$('.js-step-3-nav').trigger('click');
 		} else {
-	    // $('html, body').animate({
-	    //     // scrollTop: $('.js-step-2').offset().top
-	    // }, 500);
+		  $('html, body').animate({
+	    	scrollTop: $('.js-step-2 .with-errors ul').closest('article').offset().top
+	      }, 500);
 		}
 	});
 
@@ -143,24 +168,66 @@ $(document).ready(function(){
 		var matchName = $(this).data('name')
 
 		if(isChecked) {
+			checkedOptions--;
 			$('.js-' + matchName).slideDown();
 		} else {
+			checkedOptions++;
 			$('.js-' + matchName).slideUp();
-			$('.js-' + matchName + ' input').prop('checked', false);
 		}
-	});
+		if(checkedOptions == 0) {
+			$('.sub-question.title').slideUp();
+			$('#js-social-checkbox').next('.with-errors').html('<ul class="list-unstyled"><li>Please select at least one platform</li></ul>');
+		} else {
+			$('.sub-question.title').slideDown();
+			$('#js-social-checkbox').next('.with-errors').html('');
+		}
 
+	});
+	$('.js-sub-question input').on('click',function(){
+		$(this).parents('article').find('.with-errors').html('');
+	});
 });
 
 /*	=Function
 -------------------------------------------------- */
+function validateStep2() {
+	if(!$('.js-social').is(':checked')) {
+		$('#js-social-checkbox').next('.with-errors').html('<ul class="list-unstyled"><li>Please select at least one platform</li></ul>');
+	} else {
+		$('#js-social-checkbox').next('.with-errors').html('');
+	}
+	$('.tag-form-group').each(function() {
+		if($(this).find('.tag').length == 0) {
+			$(this).next('.with-errors').html('<ul class="list-unstyled"><li>Please input at least one tag</li></ul>');
+		} else {
+			$(this).next('.with-errors').html('');
+		}
+	});
+	$('#js-social-checkbox input:checkbox:checked').each(function () {
+		var platform = $(this).attr('data-name');
+
+	    $('article.'+ platform).each(function () {
+			var checked = false;
+			$(this).find('input:checked').each(function () {
+				checked = true;
+				return false;
+			});
+			if(!checked) {
+				$(this).find('.with-errors').html('<ul class="list-unstyled"><li>Please select at least one platform</li></ul>');
+			} else {
+				$(this).find('.with-errors').html('');
+			}
+		});
+	});
+}
+
 function validateSelection() {
 	var city = $('.js-countries input').val();
 	var gender = $('.js-gender option:selected').val();
 
 	if(city == '')
-		$('.js-countries').next('.with-errors').text('Please select country');
+		$('.js-countries').next('.with-errors').html('<ul class="list-unstyled"><li>Please select country</li></ul>');
 
 	if(gender == '')
-		$('.js-gender').next('.with-errors').text('Please select gender');
+		$('.js-gender').next('.with-errors').html('<ul class="list-unstyled"><li>Please select gender</li></ul>');
 }
